d7c41f4b3d4abc13050f530ba52795c00b935fa9
sriramr94 <sriramcbts@gmail.com>
2022-08-08 14:14:10 +0530

    Pdf to text init checkin

---
+++ PdftoText/pdf_to_text.py
@@ -0,0 +1,24 @@
+import PyPDF2
+
+def get_file(fname):
+    pdfpath = fname
+    pdfobj = open(pdfpath, 'rb')
+    return pdfobj
+
+def read_pdf(pdfobj):
+    pdfread = PyPDF2.PdfFileReader(pdfobj)
+    return pdfread
+
+def convert_to_text(pdfread,textfile_name='pdf_to_text.txt'):
+    for i in range(x):
+        pageObj = pdfread.getPage(i)
+        with open(textfile_name, 'a+') as f: 
+            f.write((pageObj.extractText()))
+    return textfile_name
+
+fname = 'samplePdf1.pdf'
+pdfobj = get_file(fname)
+pdfread = read_pdf(pdfobj)
+download_file = convert_to_text(pdfread)
+print(download_file)
+
