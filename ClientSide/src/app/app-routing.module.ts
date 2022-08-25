2719b80c4866fe54b11e21c3387194ad7ed215e0
Savy <58583410+righteous93@users.noreply.github.com>
2022-08-25 23:48:38 +0530

    Pushing in the sample page with both the pages parser and pdf converter

--- ClientSide/src/app/app-routing.module.ts
+++ ClientSide/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { DestinationComponent } from './destination/destination.component';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { ParseComponent } from './parser/parser.component';
+import { PdfToTextConvertorComponent } from './pdf-to-text-convertor/pdf-to-text-convertor.component';
 
-const routes: Routes = [{path:"desination-component", component: DestinationComponent}];
+const routes: Routes=[{path:'',redirectTo:'parser',pathMatch:'full'},
+{path:'parser',component: ParseComponent},
+{path:'pdftotext',component: PdfToTextConvertorComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
