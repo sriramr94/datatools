603e24078b1ec61d499fd7e3baad77381b2fd85d
sriramr94 <sriramcbts@gmail.com>
2022-09-22 09:02:54 +0530

    Updated Search keywords function with XML functionality

--- ServerSide/Search Keywords/top_keywords.py
+++ ServerSide/Search Keywords/top_keywords.py
@@ -1,11 +1,10 @@
 import requests
-
+import xml.etree.ElementTree as ET
 def search_keyword(kw):
     url = 'https://google.com/complete/search?output=toolbar&gl=eg=&q='+str(kw)
     resp = requests.get(url)
+    mytree = ET.fromstring(resp.content)
     trend_keywords = []
-    for ele in resp.text.split('<CompleteSuggestion><suggestion data='):
-        if '<?xml version="1.0"?><toplevel>' not in ele:
-            op = ele.replace('"/></CompleteSuggestion>','').replace('"','').replace('</toplevel>','')
-            trend_keywords.append(op)
+    for ele in list(mytree):
+        trend_keywords.append(ele[0].attrib['data'])
     return trend_keywords
